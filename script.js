d979e3c71731fbf1b7c36c014c2869cda6e6f7ad
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-06-22 10:59:53 +0530

    Display of Questions randomly

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ var q1 = new Question('Who founded Facebook', ['Mark Zuckerberg','Steve Jobs','B
 var q2 = new Question('Who was the first man to step on the moon', ['Michael Collins','Neil Armstrong','Edwin Aldrid'], 1);
 
 var q3 = new Question('Which one is a reptile', ['Lion','Eagle','Snake'], 2);
+
+var questions = [q1,q2,q3];
+var n = Math.floor(Math.random()*questions.length);
+
+questions[n];
