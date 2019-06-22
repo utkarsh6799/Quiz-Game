af1b4f9f8f2e5ce49d3c4e43b45771e7b9957400
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-06-22 10:50:19 +0530

    Function to create questons

---
+++ script.js
@@ -0,0 +1,11 @@
+fuction Question(question,answers,correct){
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+var q1 = new Question('Who founded Facebook', ['Mark Zuckerberg','Steve Jobs','Bill Gates'], 0);
+
+var q2 = new Question('Who was the first man to step on the moon', ['Michael Collins','Neil Armstrong','Edwin Aldrid'], 1);
+
+var q3 = new Question('Which one is a reptile', ['Lion','Eagle','Snake'], 2);
