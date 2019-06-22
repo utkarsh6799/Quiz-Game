a2910ec221c3a934f1bb6e22272029f498582b2f
utkarsh6799 <utkarshsingh6799@gmail.com>
2019-06-22 11:27:05 +0530

    Invoked Function created

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
-function Question(question,answers,correct){
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
-
-Question.prototype.displayQuestion = 
-function(){
-    console.log(this.question);
+(function(){
+    function Question(question,answers,correct){
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
     
-    for(var i=0;i < this.answers.length; i++)
-    {
-        console.log(i+ ': ' + this.answers[i]);
+    Question.prototype.displayQuestion = 
+    function(){
+        console.log(this.question);
+        
+        for(var i=0;i < this.answers.length; i++)
+        {
+            console.log(i+ ': ' + this.answers[i]);
+        }
     }
-}
-
-Question.prototype.checkAnswer =
-function(ans){
-    if(ans===this.correct){
-        console.log('Correct Answer!')
+    
+    Question.prototype.checkAnswer =
+    function(ans){
+        if(ans===this.correct){
+            console.log('Correct Answer!')
+        }
+        else{
+            console.log('Wrong Answer,try again! :)');
+        }
     }
-    else{
-        console.log('Wrong Answer,try again! :)');
-    }
-}
+    
+    var q1 = new Question('Who founded Facebook', ['Mark Zuckerberg','Steve Jobs','Bill Gates'], 0);
+    
+    var q2 = new Question('Who was the first man to step on the moon', ['Michael Collins','Neil Armstrong','Edwin Aldrid'], 1);
+    
+    var q3 = new Question('Which one is a reptile', ['Lion','Eagle','Snake'], 2);
+    
+    var questions = [q1,q2,q3];
+    var n = Math.floor(Math.random()*questions.length);
+    
+    questions[n].displayQuestion();
+    
+    var answer = parseInt(prompt('Please select the correct answer'));
+    
+    questions[n].checkAnswer(answer);
 
-var q1 = new Question('Who founded Facebook', ['Mark Zuckerberg','Steve Jobs','Bill Gates'], 0);
+})();
 
-var q2 = new Question('Who was the first man to step on the moon', ['Michael Collins','Neil Armstrong','Edwin Aldrid'], 1);
-
-var q3 = new Question('Which one is a reptile', ['Lion','Eagle','Snake'], 2);
-
-var questions = [q1,q2,q3];
-var n = Math.floor(Math.random()*questions.length);
-
-questions[n].displayQuestion();
-
-var answer = parseInt(prompt('Please select the correct answer'));
-
-questions[n].checkAnswer(answer);
